d041c251bb47999e4184907a05d7c2ea9dd5b792
Teflon Ted <github@rudiment.net>
2008-12-03 22:00:36 -0500

    DRY'd up all the paths

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,24 +2,32 @@ role :site, 'pocketrails.com'
 role :local, 'localhost'
 
 namespace :pr do
+  
+  desktop_path = "/Users/ted/Desktop"
+  rails_path = "#{desktop_path}/rails"
+  rdoc_path = "#{rails_path}/doc/rdoc"
+  project_path = "#{desktop_path}/iphone_rdoc_template"
+  remote_path = "~/pocketrails.com"
+  zipped_file = "rdoc.gz"
+  
   desc "Generate the RDoc."
   task :rdoc, :roles => :local do
-    run "cd ~/Desktop/rails && rake rerdoc template=~/Desktop/iphone_rdoc_template/lib/iphone_rdoc_template"
+    run "cd #{rails_path} && rake rerdoc template=#{project_path}/lib/iphone_rdoc_template"
   end
   
   desc "Compress the files locally."
   task :gzip, :roles => :local do
-    run "cd ~/Desktop/rails/doc/rdoc && gzip --stdout --recursive * > rdoc.gz"
+    run "cd #{rdoc_path} && gzip --stdout --recursive * > #{zipped_file}"
   end
   
   desc "Copy the zip file to the server."
   task :scp, :roles => :site do
-    upload "rdoc", "~", :via => :scp
+    upload "#{rdoc_path}/rdoc.gz", "#{remote_path}", :via => :scp
   end
 
   desc "Decompress the file remotely."
   task :gunzip, :roles => :site do
-    run "cd ~/pocketrails.com && gunzip ~/rdoc.gz"
+    run "cd #{remote_path} && gunzip #{zipped_file}"
   end
 end
 
